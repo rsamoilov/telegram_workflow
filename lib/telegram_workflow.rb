76b3adb1c886847c82d0683476110b86a4726549
Roman Samoilov <nanosamoilov@gmail.com>
2020-03-22 12:11:17 +0100

    gem files

---
+++ lib/telegram_workflow.rb
@@ -0,0 +1,6 @@
+require "telegram_workflow/version"
+
+module TelegramWorkflow
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "telegram_workflow"
+
+require "minitest/autorun"
