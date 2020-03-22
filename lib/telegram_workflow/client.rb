e06df4ca334046bb4ae19dfd308bae2a92a9cb1d
Roman Samoilov <nanosamoilov@gmail.com>
2020-03-22 12:15:51 +0100

    Add API client

---
+++ lib/telegram_workflow/client.rb
@@ -0,0 +1,101 @@
+class TelegramWorkflow::Client
+  AVAILABLE_ACTIONS = %i(
+    setWebhook
+    deleteWebhook
+    getWebhookInfo
+
+    getMe
+    sendMessage
+    forwardMessage
+    sendPhoto
+    sendAudio
+    sendDocument
+    sendVideo
+    sendAnimation
+    sendVoice
+    sendVideoNote
+    sendMediaGroup
+    sendLocation
+    editMessageLiveLocation
+    stopMessageLiveLocation
+    sendVenue
+    sendContact
+    sendPoll
+    sendDice
+    sendChatAction
+    getUserProfilePhotos
+    getFile
+    kickChatMember
+    unbanChatMember
+    restrictChatMember
+    promoteChatMember
+    setChatAdministratorCustomTitle
+    setChatPermissions
+    exportChatInviteLink
+    setChatPhoto
+    deleteChatPhoto
+    setChatTitle
+    setChatDescription
+    pinChatMessage
+    unpinChatMessage
+    leaveChat
+    getChat
+    getChatAdministrators
+    getChatMembersCount
+    getChatMember
+    setChatStickerSet
+    deleteChatStickerSet
+    answerCallbackQuery
+    setMyCommands
+    getMyCommands
+
+    editMessageText
+    editMessageCaption
+    editMessageMedia
+    editMessageReplyMarkup
+    stopPoll
+    deleteMessage
+
+    sendSticker
+    getStickerSet
+    uploadStickerFile
+    createNewStickerSet
+    addStickerToSet
+    setStickerPositionInSet
+    deleteStickerFromSet
+    setStickerSetThumb
+
+    answerInlineQuery
+
+    sendInvoice
+    answerShippingQuery
+    answerPreCheckoutQuery
+
+    setPassportDataErrors
+
+    sendGame
+    setGameScore
+    getGameHighScores
+  )
+
+  AVAILABLE_ACTIONS.each do |action|
+    method_name = action.to_s.gsub(/([a-z\d])([A-Z])/, '\1_\2').downcase
+
+    define_method(method_name) do |params = {}|
+      make_request(action, params)
+    end
+  end
+
+  def initialize(chat_id)
+    @chat_id = chat_id
+    @webhook_url = TelegramWorkflow.config.webhook_url
+    @api_url = "https://api.telegram.org/bot#{TelegramWorkflow.config.api_token}"
+  end
+
+  private
+
+  def make_request(action, params)
+    response = HTTP.post("#{@api_url}/#{action}", json: { chat_id: @chat_id, **params })
+    response.parse
+  end
+end
