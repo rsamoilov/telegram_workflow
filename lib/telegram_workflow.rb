83199ff479c86814109c7cd77b81914fe83c95a3
Roman Samoilov <nanosamoilov@gmail.com>
2020-03-29 19:53:12 +0100

    Require gem files

--- lib/telegram_workflow.rb
+++ lib/telegram_workflow.rb
@@ -1,6 +1,13 @@
-require "telegram_workflow/version"
+require "http"
 
 module TelegramWorkflow
-  class Error < StandardError; end
-  # Your code goes here...
 end
+
+require "telegram_workflow/action"
+require "telegram_workflow/client"
+require "telegram_workflow/config"
+require "telegram_workflow/errors"
+require "telegram_workflow/params"
+require "telegram_workflow/session"
+require "telegram_workflow/version"
+require "telegram_workflow/workflow"
