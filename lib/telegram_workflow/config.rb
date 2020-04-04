2788d5f758fb8d3c61d2672af9e1ab82df2c7888
Roman Samoilov <nanosamoilov@gmail.com>
2020-04-04 11:29:21 +0100

    Add initial configuration

--- lib/telegram_workflow/config.rb
+++ lib/telegram_workflow/config.rb
@@ -5,10 +5,21 @@ module TelegramWorkflow
     def configure
       @config ||= Configuration.new
       yield(@config)
+      @__after_configuration.call
+    end
+
+    def __after_configuration(&block)
+      @__after_configuration = block
     end
   end
 
   class Configuration
     attr_accessor :session_store, :logger, :client, :start_action, :webhook_url, :api_token
+
+    def initialize
+      @session_store = Rails.cache
+      @logger = Rails.logger
+      @client = TelegramWorkflow::Client
+    end
   end
 end
