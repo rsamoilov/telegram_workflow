01537cd45f4fc063fef6210383a5037118053b20
Roman Samoilov <nanosamoilov@gmail.com>
2020-04-11 16:25:17 +0100

    Add double redirect tests

--- lib/telegram_workflow/errors.rb
+++ lib/telegram_workflow/errors.rb
@@ -1,4 +1,7 @@
 module TelegramWorkflow::Errors
   class DoubleRedirect < StandardError
+    def initialize(msg = "Redirect was called multiple times in the step callback.")
+      super
+    end
   end
 end
---
+++ spec/workflow/double_redirect_spec.rb
@@ -0,0 +1,28 @@
+require "support/workflow_context"
+
+module DoubleRedirectSpec
+  class StartAction < TelegramWorkflow::Action
+    def initial
+      on_message do
+        redirect_to NextAction
+        redirect_to :next_step
+      end
+    end
+
+    def next_step
+    end
+  end
+
+  class NextAction < TelegramWorkflow::Action
+    def initial
+    end
+  end
+end
+
+RSpec.describe TelegramWorkflow::Workflow do
+  include_context "set up workflow", start_action: DoubleRedirectSpec::StartAction
+
+  it "raises an exception" do
+    expect { subject.process }.to raise_error(TelegramWorkflow::Errors::DoubleRedirect)
+  end
+end
