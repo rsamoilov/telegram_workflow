f380fe63949b2e634deab652c33e0e0f1a10bafd
Roman Samoilov <nanosamoilov@gmail.com>
2020-04-18 12:08:04 +0100

    Enhance params test coverage

--- spec/params_spec.rb
+++ spec/params_spec.rb
@@ -24,11 +24,132 @@ RSpec.describe TelegramWorkflow::Params do
       expect(subject.message_text).to eq("test message")
     end
 
-    it "correctly checks for start message" do
-      expect(subject.start?).to be(false)
+    it "doesn't fail in case there's no message" do
+      params[:message].delete(:text)
+      expect(subject.message_text).to be_nil
+    end
+  end
 
+  context "#start" do
+    let!(:params) do
+      {
+        message: {}
+      }
+    end
+
+    it "correctly identifies start message" do
       params[:message][:text] = "/start"
-      expect(subject.start?).to be(true)
+      expect(subject).to be_start
+
+      params[:message][:text] = "/startgroup"
+      expect(subject).to be_start
+
+      params[:message][:text] = "/start PAYLOAD"
+      expect(subject).to be_start
+
+      params[:message][:text] = "/startgroup PAYLOAD"
+      expect(subject).to be_start
+    end
+
+    it "correctly identifies a message with /start substring" do
+      params[:message][:text] = "TEST /start MESSAGE"
+      expect(subject).not_to be_start
+    end
+
+    it "correctly identifies a non-start message" do
+      params[:message][:text] = "test message"
+      expect(subject).not_to be_start
+    end
+
+    it "doesn't fail in case there's no message" do
+      params[:message].delete(:text)
+      expect(subject).not_to be_start
+    end
+  end
+
+  context "#deep_link_payload" do
+    let!(:params) do
+      {
+        message: {}
+      }
+    end
+
+    it "correctly returns deep-link payload from start message" do
+      params[:message][:text] = "/start PAYLOAD"
+      expect(subject.deep_link_payload).to eq("PAYLOAD")
+    end
+
+    it "correctly returns deep-link payload from startgroup message" do
+      params[:message][:text] = "/startgroup PAYLOAD"
+      expect(subject.deep_link_payload).to eq("PAYLOAD")
+    end
+
+    it "correctly returns deep-link payload with spaces" do
+      payload = "THIS IS A TEST PAYLOAD"
+      params[:message][:text] = "/start #{payload}"
+      expect(subject.deep_link_payload).to eq(payload)
+    end
+
+    it "doesn't return payload from non-start messages" do
+      params[:message][:text] = "test message"
+      expect(subject.deep_link_payload).to be_nil
+
+      params[:message][:text] = "/test_command PAYLOAD"
+      expect(subject.deep_link_payload).to be_nil
+
+      params[:message][:text] = "TEST /start MESSAGE"
+      expect(subject.deep_link_payload).to be_nil
+    end
+
+    it "returns nil on start message without payload" do
+      params[:message][:text] = "/start"
+      expect(subject.deep_link_payload).to be_nil
+    end
+
+    it "returns nil on startgroup message without payload" do
+      params[:message][:text] = "/startgroup"
+      expect(subject.deep_link_payload).to be_nil
+    end
+
+    it "returns empty string in case there's no message" do
+      params[:message].delete(:text)
+      expect(subject.deep_link_payload).to be_nil
+    end
+  end
+
+  context "#command" do
+    let!(:params) do
+      {
+        message: {}
+      }
+    end
+
+    it "correctly identifies start command" do
+      params[:message][:text] = "/start"
+      expect(subject).to be_command
+
+      params[:message][:text] = "/start PAYLOAD"
+      expect(subject).to be_command
+    end
+
+    it "correctly identifies a command" do
+      params[:message][:text] = "/get_stats"
+      expect(subject).to be_command
+    end
+
+    it "correctly identifies a message" do
+      params[:message][:text] = "a message"
+      expect(subject).not_to be_command
+    end
+
+    it "correctly identifies a message with slashes" do
+      params[:message][:text] = "a /test message"
+      expect(subject).not_to be_command
+    end
+
+    it "doesn't fail in case there's no message" do
+      params[:message].delete(:text)
+      expect(subject).not_to be_command
     end
   end
 
@@ -43,6 +164,11 @@ RSpec.describe TelegramWorkflow::Params do
 
     it "returns the callback data" do
       expect(subject.callback_data).to eq("callback_1")
+    end
+
+    it "returns nil in case there's no callback data" do
+      params.delete(:callback_query)
+      expect(subject.callback_data).to be_nil
     end
   end
 
